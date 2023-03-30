30afd366f5b7b2af1c10572ecfb57313e1708fb7
Walid-Elass <walid.el-ass.1@ulaval.ca>
2023-03-29 20:23:38 -0400

    Finalized User endpoints

--- api/api_v1/handlers/user.py
+++ api/api_v1/handlers/user.py
@@ -1,5 +1,7 @@
-from fastapi import APIRouter, HTTPException, status
-from schemas.user_schema import  UserAuth, UserOut
+from fastapi import APIRouter, Depends, HTTPException, status
+from api.dependencies.user_dependencies import get_current_user
+from models.user_model import User
+from schemas.user_schema import  UserAuth, UserOut, UserUpdate
 from services.user_service import UserService
 
 import pymongo
@@ -14,4 +16,19 @@ async def create_user(data: UserAuth):
         raise HTTPException (
             status_code=status.HTTP_400_BAD_REQUEST,
             detail = "User with this email or username already exists"
+        )
+    
+@user_router.get('/me',summary="Get details of currently logged user", response_model=UserOut)
+async def retreive_current_user(user: User = Depends(get_current_user)):
+    return user
+         
+
+@user_router.put('/update', summary="Update a user's information", response_model=UserOut)
+async def update_user(data: UserUpdate, user : User = Depends(get_current_user)):
+    try:
+        return await UserService.update_user(user.id, data)
+    except pymongo.errors.OperationFailure:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="User does not exist"
         )
--- schemas/user_schema.py
+++ schemas/user_schema.py
@@ -8,6 +8,11 @@ class UserAuth(BaseModel):
     username: str = Field(..., min_length=5, max_length=50, description="User username")
     password:  str = Field(..., min_length=6, max_length=24, description="User password")
 
+class UserUpdate(BaseModel):
+    email: Optional[EmailStr] = Field(..., description="User email")
+    username: Optional[str] = Field(..., min_length=5, max_length=50, description="User username")
+    password:  Optional[str] = Field(..., min_length=6, max_length=24, description="User password")
+
 class UserOut(BaseModel):
     user_id: UUID
     email: EmailStr
